2d2a33c1fc3570544e8c3e1461c5321d864c5f36
Jesus <141883724+JesusMedina21@users.noreply.github.com>
2024-10-25 13:06:38 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -4,13 +4,17 @@ from django.utils.translation import gettext_lazy as _
 import random
 # Create your models here.
 
+#El modelo es la base de datos, aqui cree un validador para escribir solo numeros 
+
 def validate_only_letters_and_spaces(value):
     if not value.replace(" ", "").isalpha():
         raise ValidationError(
             _('El campo título de la materia solo puede contener letras y espacios.'),
             code='invalid'
         )
+#Esta es la tabla con los campos
 
 class Instrumento(models.Model):
     Nombre_Del_Instrumento = models.CharField(max_length=100,  unique=True, validators=[validate_only_letters_and_spaces])
     Familia_Del_Instrumento = models.CharField(max_length=50)
+    foto = models.CharField(max_length=1000,  unique=True, null=True)
