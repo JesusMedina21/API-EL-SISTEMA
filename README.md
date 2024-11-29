9fd6f01d00ee607ea4506e4dcb4e21b73bf8d614
Jesus <141883724+JesusMedina21@users.noreply.github.com>
2024-11-29 19:12:27 -0400

    Fino

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # API-EL-SISTEMA
+ Api Informativa sobre los Instrumentos Musicales impartidos en el Sistema Nacional de Orquestas y Coros Infantiles y Juveniles de Venezuela para aprobar la 2da parte del curso Programacion FullStack en la UNET
+
+Librerias:
  
+ pip install django
+pip install djangorestframework
+pip install django-cors-headers
+pip install coreapi
+ python manage.py makemigrations
+ python manage.py migrate
+
+para correr el proyecto
+
+python manage.py runserver
---
+++ api/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Instrumento
+from .serializer import InstrumentoSerializer
+from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from rest_framework.permissions import BasePermission
+from .permissions import IsAuthenticatedOrReadOnly 
+from django.contrib.auth import login, logout as auth_logout, authenticate  
+
+# Esta clase es para que la API convierta el codigo a JSON.
+class InstrumentoViewSet(viewsets.ModelViewSet):
+    queryset = Instrumento.objects.all()
+    serializer_class = InstrumentoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly] 
+    
+#Pagina de inicio 
+def inicio(request):
+    return render(request, 'home.html')
+
+def logout_view(request): 
+    auth_logout(request) 
+    return redirect('inicio')
