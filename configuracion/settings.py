d3b2ea058d0a24d45579736c5eb64032925e7201
Jesus <141883724+JesusMedina21@users.noreply.github.com>
2024-11-29 23:44:20 -0400

    Fino

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -256,7 +256,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Caracas'
 
